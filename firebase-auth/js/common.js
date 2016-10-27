57d5c12d97ca3319c120efc9fb3e94ac5def745f
Jenny Lee <hjl422@uw.edu>
2016-10-27 12:18:34 -0700

    first commit

--- firebase-auth/js/common.js
+++ firebase-auth/js/common.js
@@ -1,1 +1,10 @@
 //put your Firebase config and initialization code here
+var config = {
+    apiKey: "AIzaSyDEss9Z-beECjgzp4k9llQxXhZ2cVOsZZU",
+    authDomain: "tasks-demo-8cec6.firebaseapp.com",
+    databaseURL: "https://tasks-demo-8cec6.firebaseio.com",
+    storageBucket: "tasks-demo-8cec6.appspot.com",
+    messagingSenderId: "469374009140"
+};
+firebase.initializeApp(config);
+
--- firebase-auth/js/index.js
+++ firebase-auth/js/index.js
@@ -10,7 +10,14 @@ signUpForm.addEventListener("submit", function(evt) {
     //use Firebase to sign in 
     //using the email name and password...
 
-
+    firebase.auth().signInWithEmailAndPassword(emailInput.value, passwordInput.value)
+        .then(function() {
+            window.location = "secure.html";
+        })
+        .catch(function(err) {
+            alert(err.message);
+        });
+    
 
     return false;
 });
